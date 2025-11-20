a6815e5d4fc9cbe4e240d92d617fa2456212efc1
Rahul2271 <rraahhuullch@gmail.com>
2025-11-20 12:00:46 +0530

    s

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import { Analytics } from "@vercel/analytics/next"
+import Script from "next/script"
 import Navigation from "@/components/navigation"
 import Footer from "@/components/footer"
 import "./globals.css"
@@ -25,7 +26,7 @@ export const metadata: Metadata = {
       },
     ],
   },
-    generator: 'v0.app'
+  generator: "v0.app",
 }
 
 export default function RootLayout({
@@ -35,7 +36,35 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`font-sans antialiased flex flex-col min-h-screen`}>
+      <head>
+        {/* ⭐ Meta Pixel Code */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '4255417668066921');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
+        {/* ⭐ NoScript fallback */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=4255417668066921&ev=PageView&noscript=1"
+          />
+        </noscript>
+      </head>
+
+      <body className="font-sans antialiased flex flex-col min-h-screen">
         <Navigation />
         <main className="flex-1">{children}</main>
         <Footer />
